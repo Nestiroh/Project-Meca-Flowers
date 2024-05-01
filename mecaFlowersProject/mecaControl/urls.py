eced3197b59e8d0f728dca61b229f87f2e6516f5
Nestor Rodriguez <nestirodriguez89@gmail.com>
2024-05-01 15:43:39 -0500

    Django primer subida

---
+++ mecaFlowersProject/mecaControl/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login, name='login'),
+
+    path('', views.menu_inicio, name='menu_inicio'),
+
+    path('productos/', views.productos, name='productos'),
+
+    path('instalaciones/', views.instalaciones, name='instala'),
+
+    path('contactenos/', views.contactenos, name='contactenos'),
+
+    path('clasificacion/', views.clasifica, name='clasifica'),
+    
+]
---
+++ mecaFlowersProject/mecaControl/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def login(request):
+    return render(request, 'Usuario.html', {})
+
+def menu_inicio(request):
+    return render(request, 'PaginaPrincipal.html', {})
+
+def productos(request):
+    return render(request, 'Productos.html', {})
+
+def instalaciones(request):
+    return render(request, 'Instalaciones.html', {})
+
+def contactenos(request):
+    return render(request, 'Contactenos.html', {})
+
+def clasifica(request):
+    return render(request, 'Clasificacion_P.html', {})
